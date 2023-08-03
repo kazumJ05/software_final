ad5dcfe90cb3370b3389ce96802586d7bab5389f
Kazumasa Ohara <oha14kazu@yahoo.co.jp>
2023-08-03 15:33:44 +0900

    add input Roomsize

--- RoomSimulator.java
+++ RoomSimulator.java
@@ -7,9 +7,14 @@
  *
  * Copyright Kazumasa Ohara
  */
+import java.util.Scanner;
 
 public class RoomSimulator {
     public static void main(String[] args){
-        
+        Scanner stdIn = new Scanner(System.in);
+        int tatami = 0;
+
+        System.out.print("部屋の大きさは何畳半:");
+        tatami = stdIn.nextInt();
     }
 }
